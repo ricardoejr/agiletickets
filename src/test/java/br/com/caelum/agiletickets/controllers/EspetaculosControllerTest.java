7b352646986aad4c0d99150095f8e7e503d7d4c8
ricardo <ricardoejr@gmail.com>
2013-08-30 10:00:35 -0300

    Criacao de sessoes

--- src/test/java/br/com/caelum/agiletickets/controllers/EspetaculosControllerTest.java
+++ src/test/java/br/com/caelum/agiletickets/controllers/EspetaculosControllerTest.java
@@ -6,8 +6,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
-import org.joda.time.LocalDate;
-import org.joda.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -17,14 +15,12 @@ import org.mockito.Spy;
 import br.com.caelum.agiletickets.domain.Agenda;
 import br.com.caelum.agiletickets.domain.DiretorioDeEstabelecimentos;
 import br.com.caelum.agiletickets.models.Espetaculo;
-import br.com.caelum.agiletickets.models.Periodicidade;
 import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.caelum.vraptor.util.test.MockValidator;
 import br.com.caelum.vraptor.validator.ValidationException;
-import br.com.caelum.vraptor.validator.ValidationMessage;
 
 public class EspetaculosControllerTest {
 
